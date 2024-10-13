a46c8b4bb4d695d2050eb99d1e3b93dd683488d1
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-10-12 20:16:57 -0700

    update the App file draft

--- App.js
+++ App.js
@@ -1,1 +1,17 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from './context/ThemeContext';
+import { DataProvider } from './context/DataContext';
+import AppNavigator from './navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <DataProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </DataProvider>
+    </ThemeProvider>
+  );
+}
